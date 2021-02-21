f687ee39e391e407a36bc74297f3d5b22447abb8
glenn.duncan <glenn.duncan@incoherentstudios.com>
2021-02-21 21:29:58 +1100

    Test C++20 on lgtm

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
-// Adding a change to force a pull request review
+#include <span>
+#include <cstdio>
 
-int main() {}
+int main(int argc, char *argv[]) {
+
+    std::span<char*, std::dynamic_extent> args(argv, argc);
+
+    for (const auto *arg : args) {
+        std::puts(arg);
+    }
+
+}
